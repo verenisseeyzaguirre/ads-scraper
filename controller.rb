14b2377eca9da116e329918fa288215ac6592c20
Claudia Luján <claudia.lujan@ms4m.com>
2022-03-09 04:53:26 -0500

    add Controller

---
+++ controller.rb
@@ -0,0 +1,29 @@
+require_relative "scraper_all_ads_service"
+require_relative "realestate"
+require 'json'
+
+class Controller
+  def initialize(id)
+    @realestate = Realestate.new(id)
+  end
+
+  def run
+    ads = ScraperAllAdsService.new(@realestate.id).call
+    if ads.nil?
+      puts "Argumento ingresado no válido"
+      return
+    end
+    @realestate.set_ads(ads)
+    catalog_json = @realestate.to_json
+    ads_count = catalog_json.size
+    if ads_count == 0
+      puts "No hay anuncios"
+    elsif ads_count == 1
+      puts "Guardando 1 anuncio..."
+    else
+      puts "Guardando #{ads_count} anuncios..."
+    end
+    File.write("./#{@realestate.id}.json", JSON.dump(catalog_json))
+    puts "Archivo #{@realestate.id}.json creado"
+  end
+end
